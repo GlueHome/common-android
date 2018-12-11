37ed872b8d273c97f4d9c93ace3a653b350f7abf
Cesar Ferreira <cesar.manuel.ferreira@gmail.com>
2018-12-11 18:50:42 +0000

    Added time delta

---
+++ domain/src/main/kotlin/com/gluehome/common/domain/utils/TimeDelta.kt
@@ -0,0 +1,24 @@
+package com.gluehome.common.domain.utils
+
+import java.util.*
+
+/**
+ * Calculate time differences
+ */
+class TimeDelta(private val startTime: Date = Date()) {
+
+    private lateinit var endTime: Date
+
+    fun finish(endTime: Date = Date()) {
+        this.endTime = endTime
+    }
+
+    /**
+     * Diference
+     */
+    var delta: Long = 0
+        get() = endTime.time - startTime.time
+
+    var deltaInSeconds: Long = 0
+        get() = delta / 1000
+}
