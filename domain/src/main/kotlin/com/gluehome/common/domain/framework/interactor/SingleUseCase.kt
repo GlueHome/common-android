2157fe797e26e93dac564ab8e980f8e166d2172e
Cesar Ferreira <cesar.manuel.ferreira@gmail.com>
2018-12-17 12:22:17 +0000

    Removed either from the single use case

--- domain/src/main/kotlin/com/gluehome/common/domain/framework/interactor/SingleUseCase.kt
+++ domain/src/main/kotlin/com/gluehome/common/domain/framework/interactor/SingleUseCase.kt
@@ -1,17 +1,15 @@
 package com.gluehome.common.domain.framework.interactor
 
-import com.gluehome.common.domain.exceptions.Failure
-import com.gluehome.common.domain.framework.functional.Either
 import com.gluehome.common.domain.framework.threads.RxThreads
 import io.reactivex.Single
 
 abstract class SingleUseCase<Type, in Params>(private val threads: RxThreads) where Type : Any {
 
-    abstract fun run(params: Params): Single<Either<Failure, Type>>
+    abstract fun run(params: Params): Single<Type>
 
-    open operator fun invoke(params: Params): Single<Either<Failure, Type>> {
+    open operator fun invoke(params: Params): Single<Type> {
         return run(params)
-                .subscribeOn(threads.io())
-                .observeOn(threads.ui())
+            .subscribeOn(threads.io())
+            .observeOn(threads.ui())
     }
 }
--- presentation/src/main/kotlin/com/gluehome/common/presentation/logs/LineNumberDebugTree.kt
+++ presentation/src/main/kotlin/com/gluehome/common/presentation/logs/LineNumberDebugTree.kt
@@ -4,6 +4,10 @@ import timber.log.Timber
 
 class LineNumberDebugTree : Timber.DebugTree() {
     override fun createStackElementTag(element: StackTraceElement): String? {
+        String.format("%s:%s",
+            element.getLineNumber(),
+            super.createStackElementTag(element)
+        );
         return "(${element.fileName}:${element.lineNumber})#${element.methodName}"
     }
 }
