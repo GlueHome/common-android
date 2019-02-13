7114694401d3dbd5a8e08872bc744ddc000af11d
Cesar Ferreira <cesar.manuel.ferreira@gmail.com>
2019-02-13 11:36:56 +0000

    added multiple constuctor to spaces item decoration

--- presentation/src/main/kotlin/com/gluehome/common/presentation/framework/ui/SpacesItemDecoration.kt
+++ presentation/src/main/kotlin/com/gluehome/common/presentation/framework/ui/SpacesItemDecoration.kt
@@ -4,16 +4,35 @@ import android.graphics.Rect
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 
-class SpacesItemDecoration(private val space: Int) : RecyclerView.ItemDecoration() {
+class SpacesItemDecoration : RecyclerView.ItemDecoration {
+
+    private val top: Int
+    private val right: Int
+    private val bottom: Int
+    private val left: Int
+
+    constructor(space: Int) : super() {
+        this.top = space
+        this.right = space
+        this.bottom = space
+        this.left = space
+    }
+
+    constructor(top: Int, right: Int, bottom: Int, left: Int) : super() {
+        this.top = top
+        this.right = right
+        this.bottom = bottom
+        this.left = left
+    }
 
     override fun getItemOffsets(outRect: Rect, view: View, parent: RecyclerView, state: RecyclerView.State) {
-        outRect.left = space
-        outRect.right = space
-        outRect.bottom = space
+        outRect.left = left
+        outRect.right = right
+        outRect.bottom = bottom
 
         // Add top margin only for the first item to avoid double space between items
         if (parent.getChildAdapterPosition(view) == 0) {
-            outRect.top = space
+            outRect.top = top
         }
     }
 }
