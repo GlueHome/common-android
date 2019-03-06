79a1280fa302f69bae096f9b029e82b03b3c1841
Cesar Ferreira <cesar.manuel.ferreira@gmail.com>
2019-03-06 14:51:07 +0000

    added our version of logsene

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -1,15 +1,5 @@
-object Releases {
-
-    private const val versionMajor = 1
-    private const val versionMinor = 0
-    private const val versionPatch = 0
-
-    const val version_code = versionMajor * 100000 + versionMinor * 100 + versionPatch
-    const val version_name = "$versionMajor.$versionMinor.$versionPatch"
-}
-
 object Versions {
-    const val kotlin = "1.3.11"
+    const val kotlin = "1.3.21"
 
     const val gradle = "3.2.1"
 
