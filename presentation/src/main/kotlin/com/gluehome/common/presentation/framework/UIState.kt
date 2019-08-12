fce6af76d077759930599c1846a74caf975a0164
Cesar Ferreira <cesar.manuel.ferreira@gmail.com>
2019-08-12 12:08:56 +0100

    Added UI State

---
+++ presentation/src/main/kotlin/com/gluehome/common/presentation/framework/UIState.kt
@@ -0,0 +1,10 @@
+package com.gluehome.common.presentation.framework
+
+import com.gluehome.common.domain.exceptions.Failure
+
+sealed class UIState {
+    object Loading : UIState()
+    object Empty : UIState()
+    data class Problem(val error: Failure) : UIState()
+    data class Success<T>(val data: T) : UIState()
+}
