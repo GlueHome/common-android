3bcb4ca1a40f62da6b6d30314899a9b05076d5c6
Cesar Ferreira <cesar.manuel.ferreira@gmail.com>
2020-07-20 11:52:32 +0100

    enforce locale on the timestamp generator

--- domain/src/main/kotlin/com/gluehome/common/domain/framework/date/TimestampProvider.kt
+++ domain/src/main/kotlin/com/gluehome/common/domain/framework/date/TimestampProvider.kt
@@ -1,13 +1,12 @@
 package com.gluehome.common.domain.framework.date
 
 import java.text.SimpleDateFormat
-import java.util.Date
-import java.util.TimeZone
+import java.util.*
 
 class TimestampProvider {
 
     fun generateIsoFormattedTimestamp(date: Date): String {
-        return SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'")
+        return SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'", Locale.UK)
             .apply {
                 timeZone = TimeZone.getTimeZone("UTC")
             }.format(date)
